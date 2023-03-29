8e0436322a506fced02a688c5d53d4d36ff9eae8
0wew0 <wang124yi@163.com>
2023-03-29 15:41:33 +0800

    Update mytranslate.dart

--- lib/i18n/mytranslate.dart
+++ lib/i18n/mytranslate.dart
@@ -3,16 +3,25 @@ import 'package:bluetooth_test/i18n/en.dart';
 import 'package:bluetooth_test/i18n/zhcn.dart';
 
 String tt(String key) {
-  Map myi18nMap = {'en': en, 'zh-CN': zhcn};
-  List tarr = [translate('loc')];
+  Map tmap = {};
+  switch (translate('loc')) {
+    case 'en':
+      tmap = en;
+      break;
+    case 'zh-CN':
+      tmap = zhcn;
+      break;
+    default:
+  }
+  List tarr = [];
   tarr.addAll(key.split("."));
   String returnstr = "";
   for (var i = 0; i < tarr.length; i++) {
-    if (myi18nMap.containsKey(tarr[i])) {
+    if (tmap.containsKey(tarr[i])) {
       if (tarr.length - 1 == i) {
-        returnstr = myi18nMap[tarr[i]];
+        returnstr = tmap[tarr[i]];
       } else {
-        myi18nMap = myi18nMap[tarr[i]];
+        tmap = tmap[tarr[i]];
       }
     } else {
       return key;
